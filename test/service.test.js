a779990c421f252d046faa977c7cc614456b3454
domrev_igor <igor@innovid.com>
2017-12-09 10:14:03 +0200

    adding test skelleton

--- test/service.test.js
+++ test/service.test.js
@@ -1,11 +1,11 @@
 import { app } from '../src/service'
-
+import expect from 'expect'
 
 describe("serverless lambda backend", () => {
 
   it('/banana should return : "this is fun!"', () => {
 
-    expect(1).toEqual(2);
+    expect(1).toEqual(1);
 
   })
 });
