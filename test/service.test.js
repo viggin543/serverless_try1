0b7846d79dd9f204c03fe575f46d1ebbdad7a272
domrev_igor <igor@innovid.com>
2017-12-09 09:41:07 +0200

    adding test skeleton

---
+++ test/service.test.js
@@ -0,0 +1,11 @@
+import { app } from '../src/service'
+
+
+describe("serverless lambda backend", () => {
+
+  it('/banana should return : "this is fun!"', () => {
+
+    expect(1).toEqual(2);
+
+  })
+});
