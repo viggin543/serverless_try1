a0aa50b7270ab4c24415d9dea4f9049f60ceecd9
domrev_igor <igor@innovid.com>
2018-01-20 12:33:10 +0200

    wiring up all together

--- lambda.js
+++ lambda.js
@@ -1,5 +1,5 @@
 'use strict'
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./dist/src/service')
-const server = awsServerlessExpress.createServer(app)
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./dist/service');
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
